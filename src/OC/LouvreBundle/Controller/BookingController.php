796e6d7a2667e488c2159d760d300b8a0e342663
Guillaume Pruvost <cecileguillaume@gmail.com>
2018-05-17 18:17:58 +0200

    test css et js

--- src/OC/LouvreBundle/Controller/BookingController.php
+++ src/OC/LouvreBundle/Controller/BookingController.php
@@ -10,10 +10,12 @@ namespace OC\LouvreBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class BookingController extends Controller
 {
     public function indexAction(){
         return $this->render('@OCLouvre/Booking/index.html.twig');
+
     }
 }
